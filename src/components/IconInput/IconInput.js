3d454edae3a47bb4b161d6b63862a41c8f4f1ca9
Nuno Potier <nunopotier@chinasystems.com>
2021-05-25 17:24:20 +0100

    Input and Select

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
@@ -13,7 +13,87 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const [text, setText] = useState('');
+  const Style = {
+    small: {
+      fontSize: 14,
+      bottomBorder: 1,
+      iconSize: 16,
+      iconStroke: 1,
+      paddingLeft: 24
+    },
+    large: {
+      fontSize: 18,
+      bottomBorder: 2,
+      iconSize: 24,
+      iconStroke: 2,
+      paddingLeft: 36
+    }
+  }
+  const styles = Style[size];
+
+  if (!styles) {
+    throw new Error('Please use small or large size only, following size not supported: ' + size);
+  }
+
+  return (
+    <Wrapper style={{
+      '--color': text === '' ? COLORS.gray500 : COLORS.gray700,
+      }} >
+      <IconWrapper style={{ '--size': 16 + 'px'}} >
+        <Icon id={icon} size={styles.iconSize} strokeWidth={styles.iconStroke}/>
+      </IconWrapper>
+      <TextInput 
+        style={{
+          '--font-size': styles.fontSize + 'px',
+          '--font-weight': text === '' ? 400 : 700,
+          '--border-bottom' : styles.bottomBorder + 'px',
+          '--padding-left' : styles.paddingLeft + 'px',
+          '--width': width + 'px'
+        }}
+        placeholder={placeholder}
+        label={label}
+        value={text}
+        onChange={(ev) => setText(ev.target.value)}
+      />
+      <VisuallyHidden>{label}</VisuallyHidden>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  
+  color: var(--color);
+  
+
+  :hover {
+    color: ${COLORS.black};
+  }
+  
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size)px;
+`;
+
+const TextInput = styled.input`
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+  border: none;
+  border-bottom: var(--border-bottom) solid ${COLORS.black};
+  padding-left: var(--padding-left);
+  width: var(--width);
+
+  :focus-visible {
+    outline-offset: 2px;
+  }
+  
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,34 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper value={value} onChange={onChange} style={{'--width': displayedValue.length * 10}}>
       {children}
-    </select>
+      <Icon 
+        id='chevron-down'
+        size="12"
+      />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.select`
+  width: var(--width);
+  height: 43px;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: 16px;
+  line-height: 19px;
+  border-radius: 8px;
+  border-width: 0;
+  padding: 12px 16px;
+  /* for Firefox */
+  -moz-appearance: none;
+  /* for Chrome */
+  -webkit-appearance: none;
+
+  :hover {
+    color: black;
+  }
+`;
 
 export default Select;
